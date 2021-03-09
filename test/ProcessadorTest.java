fb9005194550f81491a3186fad312ea8da9b7830
daniel.garrido <daniel.garrido@b2wdigital.com>
2021-03-09 00:23:13 -0300

    implement next test validation

--- test/ProcessadorTest.java
+++ test/ProcessadorTest.java
@@ -25,6 +25,26 @@ class ProcessadorTest {
 
         List<Boleto> listBoleto = new ArrayList<Boleto>();
 
+        listBoleto.add(bA);
+        listBoleto.add(bB);
+
+        Pagamento payment = processador.processPayment(listBoleto);
+        f.makePayment(payment);
+        Assertions.assertEquals(f.status, FaturaStatus.PAGO);
+    }
+
+    @Test
+    @DisplayName("Validate if fatura get payed correctly when boletos values are more then fatura")
+    void TestaFaturaPagaMaiorBoletos() {
+        Fatura f = new Fatura(new Date(), 1, "f2");
+        Boleto boletoA = new Boleto(new Date(), 9999);
+        Boleto boletoB = new Boleto(new Date(), 1);
+
+        List<Boleto> listBoleto = new ArrayList<Boleto>();
+
+        listBoleto.add(bA);
+        listBoleto.add(bB);
+
         Pagamento payment = processador.processPayment(listBoleto);
         f.makePayment(payment);
         Assertions.assertEquals(f.status, FaturaStatus.PAGO);
